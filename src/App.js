1c4dd6c5ced925c2c4f421d03f09251a294751e3
joycewv <114795700+joycewv@users.noreply.github.com>
2023-05-11 16:00:01 +1000

    set up routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react'
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
-import { Navbar, } from './components';
+import { Navbar, Exchanges, Homepage, CryptoDetails, Cryptocurries, News } from './components';
+import './App.css'
 
 const App = () => {
   return (
     <div className='app'>
       <div className='navbar'><Navbar /></div>
-      <div className='main'></div>
+      <div className='main'>
+        <Layout>
+          <div className='routes'>
+            <Switch>
+              <Route exact path='/'>
+                <Homepage />
+              </Route>
+              <Route exact path='/exchanges'>
+                <Exchanges />
+              </Route>
+              <Route exact path='/cryptocurries'>
+                <Cryptocurries />
+              </Route>
+              <Route exact path='/crypto/:coinId'>
+                <CryptoDetails />
+              </Route>
+              <Route exact path='/news'>
+                <News />
+              </Route>
+            </Switch>
+
+          </div>
+        </Layout>
+      </div>
       <div className='footer'></div>
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,20 +1,33 @@
 import React from 'react'
 import { Button, Menu, Typography, Avatar } from 'antd'
 import { Link } from 'react-router-dom'
-import { HomeOutlined, MoneyCollectOutlined, BulbOUtlind, FundOutlined, MenuOutlined } from '@ant-design/icons'
-import icon from '../images/cryptocurrency.png'
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
+import icon from '../images/cryptocurrency.png';
 
 const NavBar = () => {
   return (
     <div className='Nav-container'>
         <div className='Log-container'>
-            <Avatar src={'icon'} size={'large'} />
+            <Avatar src={icon} size={'large'} />
             <Typography.Title level={2} className='logo'>
                 <Link to={'/'}>Cryptoverse</Link>
             </Typography.Title>
             {/*<Button className='menu-control-container'></Button>*/}
-
         </div>
+        <Menu theme={'dark'}>
+            <Menu.Item icon={<HomeOutlined />} >
+                <Link to={'/'}>Home</Link>
+            </Menu.Item>
+            <Menu.Item icon={<FundOutlined />} >
+                <Link to={'/cryptocurrencies'}>Cryptocurrencies</Link>
+            </Menu.Item>
+            <Menu.Item icon={<MoneyCollectOutlined />} >
+                <Link to={'/exchanges'}>Exchanges</Link>
+            </Menu.Item>
+            <Menu.Item icon={<BulbOutlined />} >
+                <Link to={'/news'}>News</Link>
+            </Menu.Item>
+        </Menu>
     </div>
   )
 }
