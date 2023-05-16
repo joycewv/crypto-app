19322352eac260f33950c3886e814ae152417f1a
joycewv <114795700+joycewv@users.noreply.github.com>
2023-05-16 18:46:18 +1000

    updated homepage

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import { Statistic, Typography, Row, Col } from 'antd';
+import React from 'react';
+//import millify from 'millify';
+
+//import { Link } from 'react-router-dom';
+
+const { Title } = Typography;
 
 const Homepage = () => {
   return (
-    <div>Homepage</div>
+    <>
+    <Title level={2} className='heading'>Global Crypto Stats</Title>
+    <Row>
+      <Col span={12}><Statistic title='Total Cryptocurrentcies' value='5'/></Col>
+      <Col span={12}><Statistic title='Total Exchanges' value='5'/></Col>
+      <Col span={12}><Statistic title='Total Market Cap' value='5'/></Col>
+      <Col span={12}><Statistic title='Total 24h Volume' value='5'/></Col>
+      <Col span={12}><Statistic title='Total markets' value='5'/></Col>
+    </Row>
+    </>
   )
 }
 
