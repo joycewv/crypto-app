eb7a722f3625260cb1520ba9968cd32a78a832f6
joycewv <114795700+joycewv@users.noreply.github.com>
2023-05-22 19:24:56 +1000

    update cryptonewsapi

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,15 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { cryptoApi } from "../services/cryptoApi";
+import { cryptoNewsApi } from "../services/cryptoNewsApi";
 
 export default configureStore({
     reducer: {
         [cryptoApi.reducerPath]: cryptoApi.reducer,
+        [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
     },
     //add below middleware as apps not working shows"warning: middleware for rtk-query api at reducerpath has not been added to the store"
-    middleware: (getDefaultMiddleware) => 
-      getDefaultMiddleware().concat(cryptoApi.middleware),
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(
+        cryptoApi.middleware,
+        cryptoNewsApi.middleware,
+    )
 });
--- src/services/cryptoNewsApi.js
+++ src/services/cryptoNewsApi.js
@@ -6,7 +6,7 @@ const cryptoNewsHeaders = {
     'X-RapidAPI-Host': 'bing-news-search1.p.rapidapi.com'
 }
 
-const baseUrl ='https://bing-news-search1.p.rapidapi.com'
+const baseUrl ='https://bing-news-search1.p.rapidapi.com';
 
 const createRequest = (url) => ({ url, headers: cryptoNewsHeaders})
 
@@ -16,8 +16,8 @@ export const cryptoNewsApi = createApi({
     endpoints: (builder) => ({
         getCryptoNews: builder.query({
             query: (newsCategory, count) => createRequest(`/news/search?q=${newsCategory}&safeSearch=Off&textFormat=Raw&freshness=Day&count=${count}`),
-        })
-    })
+        }),
+    }),
 
 });
 
