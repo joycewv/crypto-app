6ce29627a9068a899bc3fc3cfbdb1f0287de3f77
joycewv <114795700+joycewv@users.noreply.github.com>
2023-05-22 11:00:13 +1000

    updated cryptocurrencies page

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,11 +3,14 @@ import React from 'react';
 import millify from 'millify';
 import { Link } from 'react-router-dom';
 import { useGetCryptosQuery } from '../services/cryptoApi';
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
+
 
 const { Title } = Typography;
 
 const Homepage = () => {
-  const { data, isFetching } = useGetCryptosQuery();
+  const { data, isFetching } = useGetCryptosQuery(10);
 
   const globalStats = data?.data?.stats;
   if (isFetching) return 'loading...';
@@ -22,11 +25,16 @@ const Homepage = () => {
       <Col span={12}><Statistic title='Total 24h Volume' value={millify(globalStats.total24hVolume)}/></Col>
       <Col span={12}><Statistic title='Total markets' value={millify(globalStats.totalMarkets)}/></Col>
     </Row>
-    <div className='home-heading-container'>
-      <title level={2} className='home-title' >Top 10 Cryptocurrencies in the world</title>
-      <title level={3} className='show-more' ><Link to='/cryptocurrencies'>Show More</Link></title>
-
+    <div className="home-heading-container">
+        <Title level={2} className='home-title'>Top 10 Cryptos In The World</Title>
+        <Title level={3} className='show-more'><Link to='/cryptocurrencies'>Show more</Link></Title>
     </div>
+    <Cryptocurrencies simplified />
+    <div className="home-heading-container">
+        <Title level={2} className='home-title'>Latest Crypto News</Title>
+        <Title level={3} className='show-more'><Link to='/news'>Show more</Link></Title>
+    </div>
+    <News simplified />
     </>
   )
 }
